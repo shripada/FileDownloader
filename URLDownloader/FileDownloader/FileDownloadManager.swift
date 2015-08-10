16e3e7fa14d54eef36614214646fe1ff9ee06145
Shripada Hebbar <shripada@robosoftin.com>
2015-08-10 10:00:58 +0530

    Update a comment

--- URLDownloader/FileDownloader/FileDownloadManager.swift
+++ URLDownloader/FileDownloader/FileDownloadManager.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 /**
-   This class creates the necessary download tasks internally for each download request
-  creates a FileDownload object and returns it to caller to perform cancel operation, or resume
-  a canceled or failed download.
+This class creates the necessary download tasks internally for each download request
+creates a FileDownload object and returns it to caller to perform cancel operation, or resume
+a canceled or failed download.
 */
 
- public class FileDownloadManager {
+public class FileDownloadManager {
 
   public typealias DownLoadCompletionHandler = (filePath: String?, success: Bool, error: NSError?)-> Void
 
-   static let sharedInstance : FileDownloadManager = {
+  static let sharedInstance : FileDownloadManager = {
     var sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
     return FileDownloadManager(configuration: sessionConfig)
-  }()
+    }()
 
   //Directory where cached files are written.
   let cacheDirectory : String = {
@@ -36,18 +36,19 @@ import Foundation
     let dir = cacheDirectory.stringByAppendingFormat("%@", bundle)
 
     return dir
-  }()
+    }()
 
-  //We want to store the last-modified date of a request, and also, the cache file path, where the pdf file is stored.
+  //We want to store the last-modified date of a request, and also, the cache file path, where the pdf file is stored. The cache gets created within the default caches folder, so we dont need to really worry
+  //about explicitely removing expired objects, or set expiry for cached objects. iOS will purge cache if need arises.
   public let cache = Cache<NSDictionary>(name: "DownloaderCache")
-  
+
   /// The underlying session.
   let session: NSURLSession
 
   /**
-    Designated initializer
-  
-    :param: configuration Session configuration
+  Designated initializer
+
+  :param: configuration Session configuration
   */
 
   required  public init(configuration: NSURLSessionConfiguration)
@@ -59,20 +60,20 @@ import Foundation
     session.invalidateAndCancel()
   }
 
-   //MARK: API
+  //MARK: API
 
-/**
+  /**
   Initiates an asynchronous download for each of the url mentioned, with a completion handler, that
   gets called for each of the successful download.
-  
+
   :param: url  The url as a String
   :param:  completion  Closure that will be called for download associated when the request is completed.
   :result: Returns the FileDownload object that encapsulates the task underneath.
-*/
+  */
 
   func download( url :String, _ completion:DownLoadCompletionHandler )-> FileDownload{
 
-    //This internally creates the download task needed. 
+    //This internally creates the download task needed.
     return FileDownload(session: session, url: url, completion)
   }
 }
